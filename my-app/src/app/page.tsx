f784537fc6ddf217b16fdfd4c1c0d424d083109e
andrewhe6 <andrewhe9264@gmail.com>
2024-10-16 15:29:56 -1000

    app
    created all necessary react/bootstrap files

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import './style.css';
+import { Container } from "react-bootstrap";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Container>
+        <h1 className="text-center">Insert page here</h1>
+      </Container>
+    </main>
+  );
+}

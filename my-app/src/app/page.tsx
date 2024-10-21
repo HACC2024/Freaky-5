cfbcbb88f01cc29d4a7ac4a764d9d6661e8f32d5
andrewhe6 <andrewhe9264@gmail.com>
2024-10-20 17:33:37 -1000

    Reorganized

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,40 +1,79 @@
 "use client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './style.css';
-import { Container } from "react-bootstrap";
-import ProgressBar from 'react-bootstrap/ProgressBar';
+import { ProgressBar, Container, Row, Col, Form, Image } from "react-bootstrap";
+import { LightningChargeFill } from "react-bootstrap-icons";
+
+/* Calculate the power usage percentage based off switches */
+function CalculatePowerUsage() {
+  return 60;
+}
+
+/* Display the power usage bar */
+const DisplayBar = () => (
+  <Container>
+    <h1 className="text-center pt-4">Power Usage</h1>
+    <LightningChargeFill size={30} color={"black"}/>
+    <ProgressBar animated now={CalculatePowerUsage()} label={`${CalculatePowerUsage()}%`} />
+  </Container>
+);
+
+/* Display the electric objects and item switches */
+const ItemSwitches = () => (
+  <Container>
+      <Row className="">
+        <Col className="col-sm my-3 mx-2">
+          <Image src="" alt="insert image" className="fluid"></Image>
+          <Form className="form-check form-switch align-items-center">
+            <input className="form-check-input mt-2" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item name</label>
+          </Form>
+        </Col>
+        <Col className="col-sm my-3 mx-2">
+          <Image src="" alt="insert image"></Image>
+          <Form className="form-check form-switch">
+            <input className="form-check-input mt-2" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item Name</label>
+          </Form>
+        </Col>
+        <Col className="col-sm my-3 mx-2">
+          <Image src="" alt="insert image"></Image>
+          <Form className="form-check form-switch">
+            <input className="form-check-input mt-2" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item Name</label>
+          </Form>
+        </Col>
+        <Col className="col-sm my-3 mx-2">
+          <Image src="" alt="insert image"></Image>
+          <Form className="form-check form-switch">
+            <input className="form-check-input mt-2" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item Name</label>
+          </Form>
+        </Col>
+        <Col className="col-sm my-3 mx-2">
+          <Image src="" alt="insert image"></Image>
+          <Form className="form-check form-switch">
+            <input className="form-check-input mt-2" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item Name</label>
+          </Form>
+        </Col>
+        <Col className="col-sm my-3 mx-2">
+          <Image src="" alt="insert image"></Image>
+          <Form className="form-check form-switch">
+            <input className="form-check-input mt-2" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item Name</label>
+          </Form>
+        </Col>
+      </Row>
+  </Container>
+);
+
+/* Home page */
 export default function Home() {
   return (
     <main>
-      <Container>
-        <h1 className="text-center">Power Usage</h1>
-        <ProgressBar now={60} label={`${60}%`} />
-      </Container>
-      <Container>
-        <div className="container">
-          <div className="row">
-            <div className="col-sm">
-              <div className="form-check form-switch">
-                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item Name</label>
-              </div>
-            </div>
-            <div className="col-sm">
-              <div className="form-check form-switch">
-                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item Name</label>
-              </div>
-            </div>
-            <div className="col-sm">
-              <div className="form-check form-switch">
-                <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-                <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Item Name</label>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Container>
-
+      <DisplayBar/>
+      <ItemSwitches/>
     </main>
   );
 }
